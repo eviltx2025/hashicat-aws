e403b679e14d0cd1f0da6e4704ada9449c93efaf
eviltx321 <135175344+eviltx321@users.noreply.github.com>
2026-01-05 17:23:46 -0600

    Add S3 bucket name and environment variables
    Added variables for S3 bucket name and deployment environment.

--- variables.tf
+++ variables.tf
@@ -48,3 +48,12 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
+variable "bucket_name" {
+  description = "The name for the S3 bucket"
+}
+
+variable "environment" {
+  description = "The environment where the resources are deployed"
+  default = "staging"
+}
